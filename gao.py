a4cf4fd09f1e8c053d43c91eaec8732c5566a8df
Ethan Moscot <ethanmoscot@gmail.com>
2023-07-07 14:04:10 -0400

    Loss graph

--- gao.py
+++ gao.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
@@ -20,9 +21,9 @@ X_train, X_val_and_test, Y_train, Y_val_and_test = train_test_split(X_scale, Y, 
 X_val, X_test, Y_val, Y_test = train_test_split(X_val_and_test, Y_val_and_test, test_size = 0.5) #equal split for validation and test sets
 
 #print(X_train.shape, X_val.shape, X_test.shape, Y_train.shape, Y_val.shape, Y_test.shape) 
-#(700, 92) (150, 92) (150, 92) (700,) (150,) (150,)
+#(700, 93) (150, 92) (150, 93) (700,) (150,) (150,)
 #training: 700 datapoints, validation and test: 150 points
-#X has 92 input features, Y only has 1
+#X has 93 input features, Y only has 1 (i.e., the results)
 
 #Convert to arrays for Keras
 X_train = np.asarray(X_train).astype('float32')
@@ -32,15 +33,28 @@ Y_val = np.asarray(Y_val).astype('float32')
 X_test = np.asarray(X_test).astype('float32')
 Y_test = np.asarray(Y_test).astype('float32')
 
-
 model = Sequential([ #two hidden layers followed by output layer
-    Dense(units=16, activation='relu', input_shape=(93,)),
-    Dense(units=16, activation='relu'),
+    Dense(units=4, activation='relu', input_shape=(93,)),
+    Dense(units=4, activation='relu'),
     Dense(units=1, activation='sigmoid')
 ])
+#y_pred = model.predict(X)
+#print(y_pred)
 
 #model.compile(optimizer='sgd', loss='binary_crossentropy', metrics=['accuracy'])
 model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mean_squared_error'])
 
-model.fit(X_train, Y_train, batch_size=16, epochs=40, validation_data=(X_val, Y_val))
-model.evaluate(X_test, Y_test)[1]
+#Fit model
+hist = model.fit(X_train, Y_train, batch_size=16, epochs=40, validation_data=(X_val, Y_val))
+#Evaluate on training and test data
+train_mse = model.evaluate(X_train, Y_train)[1]
+test_mse = model.evaluate(X_test, Y_test)[1]
+print("Training loss:", train_mse)
+print("Test loss:", test_mse)
+
+#Plot loss
+plt.title('Loss / Mean Squared Error')
+plt.plot(hist.history['loss'], label='Train')
+plt.plot(hist.history['val_loss'], label='Test')
+plt.legend()
+plt.show()

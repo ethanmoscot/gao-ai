50e6672e3ece6f5139925b985a5f522551d0270d
StephenQuirolgico <stephen.quirolgico@gmail.com>
2023-07-14 15:53:37 -0400

    Added compliant and non-compliant test cases.

--- governance_class.py
+++ governance_class.py
@@ -13,8 +13,10 @@ class GovernanceModel:
         Y = self.df['Compliance Status'].apply(lambda x: 1 if x=='Compliant' else 0)
 
         """ Use random_state=42 if needed. """
-        X_train, X_val_and_test, Y_train, Y_val_and_test = train_test_split(X, Y, test_size = 0.3) #val_and_test is 30% of dataset
-        X_val, X_test, Y_val, Y_test = train_test_split(X_val_and_test, Y_val_and_test, test_size = 0.5) #equal split for validation and test sets
+        X_train, X_val_and_test, Y_train, Y_val_and_test = \
+                train_test_split(X, Y, test_size = 0.3, random_state=42) #val_and_test is 30% of dataset
+        X_val, X_test, Y_val, Y_test = \
+                train_test_split(X_val_and_test, Y_val_and_test, test_size = 0.5, random_state=42) #equal split for validation and test sets
 
         # Some recommend using a single hidden layer where the number of nodes (units) is 
         # equal to sqrt(num_input_nodes * num_output_nodes).
@@ -31,10 +33,10 @@ class GovernanceModel:
         # stopped improving accuracy before then, reduce num epochs. If the accuracy is still improving 
         # after this, increase epochs.
         """
-        hist = self.model.fit(X_train, Y_train, batch_size=32, epochs=3, validation_split=0.1, validation_data=(X_val, Y_val))
+        hist = self.model.fit(X_train, Y_train, batch_size=32, epochs=2, validation_split=0.1, validation_data=(X_val, Y_val))
         #Evaluate on training and test data
-        #print(self.model.evaluate(X_train, Y_train)[1])
-        #print(self.model.evaluate(X_test, Y_test)[1])
+        print(self.model.evaluate(X_train, Y_train)[1])
+        print(self.model.evaluate(X_test, Y_test)[1])
         print('GovernanceModel ready.')
 
 
@@ -48,13 +50,6 @@ class GovernanceModel:
         headers.remove('Compliance Status')
         #print(f'headers: {headers}')
 
-        #27 sample inputs
-        example_data = [99,	99,	99,	84,	97,
-                        91,	99,	86,	97,	87,	
-                        82,	97,	84,	81,	90,
-                        86,	89,	83,	88,	86,
-                        87,	92,	88,	90,	98,	
-                        99,	87]
         # Don't forget to transpose the data from a column to a row.
         df2 = pd.DataFrame(data).T
         #print(df2)
@@ -65,7 +60,7 @@ class GovernanceModel:
         T = 0.5
         y_pred_bool = y_pred >= T
         result = 'Compliant' if y_pred >= T else 'Not Compliant'
-        print(f'----------------------\nExample Prediction:\n{result}')
+        print(f'----------------------\nPrediction:\n{result}')
         return int(y_pred * 100)
     
     
@@ -76,9 +71,14 @@ if __name__ == "__main__":
     compliant_data = [86, 83, 82, 80, 90, 87, 83, 90, 87,	
             80, 98, 86, 87, 80, 87, 96, 86, 99, 
             84, 98, 84, 97, 92, 86, 87, 97, 98]
+    avg = sum(compliant_data) / len(compliant_data)
+    print(f'avg compliant: {avg}')
+    governance.predict(compliant_data)   
+     
     
-    non_compliant_data = [15, 20, 34, 80, 27, 87, 83, 90, 87,	
+    non_compliant_data = [50, 20, 34, 80, 87, 87, 83, 90, 87,	
             80, 98, 86, 87, 80, 87, 96, 86, 99, 
             84, 98, 84, 97, 92, 86, 87, 97, 98]
-    
-    governance.predict(non_compliant_data)
+    avg = sum(non_compliant_data) / len(non_compliant_data)
+    print(f'avg non-compliant: {avg}')
+    governance.predict(non_compliant_data)    
